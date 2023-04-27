7f078aa5a16a27972419f27ef76cf1bbdf4866d0
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-04-27 17:36:11 +0500

    TextArea component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export {default as Heading} from '@/src/components/heading/heading';
 export {default as Text} from '@/src/components/text/text';
 export {default as Tag} from '@/src/components/tag/tag';
 export {default as Button} from '@/src/components/button/button';
-export{default as Input} from '@/src/components/input/input'
+export { default as Input } from '@/src/components/input/input';
+export {default as TextArea} from '@/src/components/text-area/text-area'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,30 @@
-import {Button, Heading, Input, Tag, Text} from "@/src/components";
-import {useState} from "react";
+import { Button, Heading, Input, Tag, Text, TextArea } from "@/src/components";
+import { useState } from "react";
 
 const Index = () => {
-    const [isClick, setIsClick] = useState(false);
+  const [isClick, setIsClick] = useState(false);
   return (
+    <div>
+      <Heading tag='h3'>Heading</Heading>
+      <Text size='l'>Text</Text>
+      <Tag size={"m"} color={'red'}>$200</Tag>
+      <Tag size="m" color="green">$900</Tag>
+      <Button appearance='primary'>Primary</Button>
+      <Button appearance='ghost'>Ghost</Button>
+      <Button appearance='ghost' arrow={isClick ? 'down' : 'right'} onClick={() => setIsClick(prev => !prev)}>
+        Arrow
+      </Button>
+      <Button appearance='primary' arrow='down'>
+        Down
+      </Button>
+      <br />
+
+
+      <Input placeholder='Enter' />
       <div>
-        <Heading tag='h3'>Heading</Heading>
-        <Text size='l'>Text</Text>
-        <Tag size={"m"} color={'red'}>$200</Tag>
-          <Button appearance='primary'>Primary</Button>
-          <Button appearance='ghost'>Ghost</Button>
-          <Button appearance='ghost' arrow={isClick ? 'down' : 'right'} onClick={() => setIsClick(prev => !prev)}>
-              Arrow
-          </Button>
-          <Button appearance='primary' arrow='down'>
-              Down
-          </Button>
-          <br/>
-
-
-          <Input placeholder='Enter'/>
+        <TextArea placeholder="Message" />
       </div>
+    </div>
   )
 }
 export default Index
