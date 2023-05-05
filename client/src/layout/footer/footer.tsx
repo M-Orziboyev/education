d062b65997691a0dfb90b4dcecc69dace6f17cfe
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-05-05 19:23:03 +0500

    footer component style

--- client/src/layout/footer/footer.tsx
+++ client/src/layout/footer/footer.tsx
@@ -1,6 +1,13 @@
 import {FooterProps} from "@/src/layout/footer/footer.props";
+import styles from './footer.module.css';
+import cn from 'classnames';
+import {format} from 'date-fns';
 
-const Footer = ({...props}: FooterProps): JSX.Element => {
-    return <div {...props}>Footer</div>
+const Footer = ({className, ...props}: FooterProps): JSX.Element => {
+    return <footer className={cn(className, styles.footer)} {...props}>
+        <div>Education &#169; {format(new Date(), 'yyyy')}. All right reserved</div>
+        <a href="#">Term of use</a>
+        <a href="#">Privacy Policy</a>
+    </footer>
 }
 export default Footer
