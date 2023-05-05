5746d8a6312fa857d3998757a84fa1b56ca55143
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-05-05 19:05:38 +0500

    Shablon && site structure

--- client/src/layout/layout.tsx
+++ client/src/layout/layout.tsx
@@ -3,19 +3,20 @@ import Sidebar from "@/src/layout/sidebar/sidebar";
 import Header from "@/src/layout/header/header";
 import Footer from "@/src/layout/footer/footer";
 import {FunctionComponent} from "react";
+import styles from "./layout.module.css"
 
-const Layout = ({children}: LayoutProps):JSX.Element => {
+const Layout = ({children}: LayoutProps): JSX.Element => {
     return <>
-        <Header />
-    <div>
-        <Sidebar />
-        <div>{children}</div>
-    </div>
-        <Footer />
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>{children}</div>
+            <Footer className={styles.footer}/>
+        </div>
     </>
 }
-export const withLayout = <T extends Record<string, unknown>> (Component: FunctionComponent<T>) => {
-    return function withLayoutComponent(props: T): JSX.Element{
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
         return (
             <Layout>
                 <Component {...props} />
