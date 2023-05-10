199b2bb3c33b491da604cfe2a4a3a7dc46c81428
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-05-10 22:10:11 +0500

    Menu component (not compelete)

--- client/src/layout/menu/menu.tsx
+++ client/src/layout/menu/menu.tsx
@@ -4,27 +4,44 @@ import {firstLevelMenu} from "@/src/helpers/constant";
 import Link from "next/link";
 import styles from "./menu.module.css"
 import cn from 'classnames'
+import {IFirstLevelMenu} from "@/src/interfaces/menu.interface";
 
 
-
-const Menu = ():JSX.Element => {
+const Menu = (): JSX.Element => {
     const {menu, firstCategory, setMenu} = useContext(AppContext)
-    const buildFirstLevel =() => {
+    const buildFirstLevel = () => {
         return (
             <>
-                {firstLevelMenu.map(c => <div key={c.route}><Link href={`/${c.route}`}>
-                    <div className={cn(styles.firstLevel, {
-                        [styles.firstLevelActive]: c.id === firstCategory,
-
-                    })}>{c.icon}
-                        <span>{c.name}</span>
-                    </div>
-                </Link></div>)}
+                {firstLevelMenu.map(c =>
+                    <div key={c.route}>
+                        <>
+                            <Link href={`/${c.route}`}>
+                                <div className={cn(styles.firstLevel, {
+                                    [styles.firstLevelActive]: c.id === firstCategory,})}>{c.icon}
+                                    <span>{c.name}</span>
+                                </div>
+                            </Link>
+                            {c.id === firstCategory && buildSecondLevel(c)}
+                        </>
+                    </div>)}
             </>
         )
     }
-    const buildSecondLevel =() => {}
-    const buildThirdLevel =() => {}
+    const buildSecondLevel = (menuItem: IFirstLevelMenu) => {
+        return(
+            <div className={styles.secondBlock}>
+                {menu.map(q => (
+                    <div key={q._id.secondCategory}>
+                        <div className={styles.secondLevel}>
+                            q._id.secondCategory
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+    const buildThirdLevel = () => {
+    }
     return <div className={styles.menu}>{buildFirstLevel()}</div>
 }
 export default Menu
