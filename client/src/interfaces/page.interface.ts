5a095a5218e337020055d2f8ffa97ae31d0f721f
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-05-10 17:36:23 +0500

    Context Provider

--- client/src/interfaces/page.interface.ts
+++ client/src/interfaces/page.interface.ts
@@ -1,21 +1,28 @@
+export enum PageCategory {
+    Courses,
+    Books,
+}
 
-export interface HhData{
-    count: string;
+export interface HhData {
+    count: number;
     juniorSalary: number;
     middleSalary: number;
     seniorSalary: number;
 }
-export interface AdvantagesData {
+
+export interface AdvatnageData {
     title: string;
     description: string;
-    _id: string;
+    id: string;
 }
+
 export interface PageModel {
     _id: string;
     alias: string;
-    title:string;
-    tags:string[];
-    description:string;
+    title: string;
+    tags: string[];
+    description: string;
     hh: HhData;
-    advantages: AdvantagesData[]
+    advantages: AdvatnageData[];
+    category: string;
 }
--- client/src/layout/layout.tsx
+++ client/src/layout/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/src/layout/header/header";
 import Footer from "@/src/layout/footer/footer";
 import {FunctionComponent} from "react";
 import styles from "./layout.module.css"
+import {AppContextProvider, IAppContext} from "@/src/context/app.context";
 
 const Layout = ({children}: LayoutProps): JSX.Element => {
     return <>
@@ -15,11 +16,13 @@ const Layout = ({children}: LayoutProps): JSX.Element => {
         </div>
     </>
 }
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
     return function withLayoutComponent(props: T): JSX.Element {
         return (
             <Layout>
-                <Component {...props} />
+                <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+                    <Component {...props} />
+                </AppContextProvider>
             </Layout>
         )
     }
