61873de698f88bdc1c8cd4de273b2878bc13080e
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-05-23 06:02:12 +0500

    Framer Motion Menu

--- client/src/layout/menu/menu.tsx
+++ client/src/layout/menu/menu.tsx
@@ -6,15 +6,38 @@ import styles from "./menu.module.css"
 import cn from 'classnames'
 import {IFirstLevelMenu, PageItem} from "@/src/interfaces/menu.interface";
 import {useRouter} from "next/router";
+import {motion} from "framer-motion"
 
 
 const Menu = (): JSX.Element => {
     const {menu, firstCategory, setMenu} = useContext(AppContext);
     const router = useRouter()
+    const variants = {
+        visible: {
+            marginBottom: 20,
+            transition: {
+                when: 'beforeChildren',
+                staggerChildren: 0.1,
+            }
+        },
+        hidden: {
+            marginBottom: 0
+        }
+    }
+    const variantsChildren = {
+        visible: {
+            opacity: 1,
+            height: 30
+        },
+        hidden: {
+            opacity: 0,
+            height: 0,
+        },
+    }
 
-    const openSecondBlock = (category:string) => {
+    const openSecondBlock = (category: string) => {
         setMenu && setMenu(menu.map(c => {
-            if (c._id.secondCategory === category){
+            if (c._id.secondCategory === category) {
                 c.isOpened = !c.isOpened;
             }
             return c
@@ -44,30 +67,32 @@ const Menu = (): JSX.Element => {
         return (
             <div className={styles.secondBlock}>
                 {menu.map(q => {
-                    if (q.pages.map(p => p._id).includes(router.asPath.split("/")[2])){
+                    if (q.pages.map(p => p._id).includes(router.asPath.split("/")[2])) {
                         q.isOpened = true;
                     }
-                        return (
-                            <div key={q._id.secondCategory}>
-                                <div className={styles.secondLevel} onClick={() => openSecondBlock(q._id.secondCategory)}>
-                                    {q._id.secondCategory}
-                                </div>
-                                <div className={cn(styles.secondLevelBlock, {
-                                    [styles.secondLevelBlockActive]: !q.isOpened,
-                                })}>
-                                    {buildThirdLevel(q.pages, menuItem.route)}
-                                </div>
+                    return (
+                        <div key={q._id.secondCategory}>
+                            <div className={styles.secondLevel} onClick={() => openSecondBlock(q._id.secondCategory)}>
+                                {q._id.secondCategory}
                             </div>
-                        )
+                            <motion.div variants={variants} layout initial={q.isOpened ? 'visible' : 'hidden'}
+                                        animate={q.isOpened ? 'visible' : 'hidden'}
+                                        className={cn(styles.secondLevelBlock)}>
+                                {buildThirdLevel(q.pages, menuItem.route)}
+                            </motion.div>
+                        </div>
+                    )
                 })}
             </div>
         )
     }
     const buildThirdLevel = (pages: PageItem[], route: string) => {
         return pages.map(p => (
-            <Link key={p._id} href={`/${route}/${p._id}`} className={cn(styles.thirdLevel, {
-                [styles.thirdLevelActive]: `/${route}/${p._id}` === router.asPath,
-            })}>{p.title}</Link>
+            <motion.div key={p._id} variants={variantsChildren}>
+                <Link href={`/${route}/${p._id}`} className={cn(styles.thirdLevel, {
+                    [styles.thirdLevelActive]: `/${route}/${p._id}` === router.asPath,
+                })}>{p.title}</Link>
+            </motion.div>
         ))
     }
     return <div className={styles.menu}>{buildFirstLevel()}</div>
