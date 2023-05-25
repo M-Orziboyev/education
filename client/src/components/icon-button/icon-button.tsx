38b81d7f4cb7aa294785eb9dcef40b634e1ec3cc
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-05-25 17:25:06 +0500

    Mobile Menu

--- client/src/components/icon-button/icon-button.tsx
+++ client/src/components/icon-button/icon-button.tsx
@@ -1,15 +1,21 @@
-import {IconButtonProps, icons} from "@/src/components/icon-button/icon-button.props";
-import button from "@/src/components/button/button";
-import styles from './icon-button.module.css'
-import cn from 'classnames'
+import { IconButtonProps, icons } from './icon-button.props';
+import styles from './icon-button.module.css';
+import cn from 'classnames';
 
-const IconButton = ({icon, appearance, className, ...props}: IconButtonProps): JSX.Element => {
-    const IconComponent = icons[icon]
-    return <button className={cn(styles.iconButton, className, {
-        [styles.primary]: appearance == 'primary',
-            [styles.white]: appearance == 'white'
-    })} {...props}>
-        <IconComponent />
-    </button>
-}
-export default IconButton
+const IconButton = ({ appearance, icon, className, ...props }: IconButtonProps): JSX.Element => {
+    const IconComponent = icons[icon];
+
+    return (
+        <button
+            className={cn(styles.iconButton, className, {
+                [styles.primary]: appearance == 'primary',
+                [styles.white]: appearance == 'white',
+            })}
+            {...props}
+        >
+            <IconComponent />
+        </button>
+    );
+};
+
+export default IconButton;
